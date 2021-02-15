589ee5ebcbcb8d75825854d5c992100800169f07
Adnan-Musinovic <adnanmusinovic9@gmail.com>
2021-02-14 16:08:28 -0800

    sticky header

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ let header = document.querySelector('.header');
 window.addEventListener('scroll', ()=> {
     header.classList.toggle('header-sticky', window.scrollY > 0);
 })
+
+let links = document.querySelectorAll('.nav-link');
+
+links.forEach(link => {
+    link.addEventListener('click', function() {
+        links.forEach(e => e.classList.remove('active'));
+        this.classList.add('active');
+    })
+})
+
+console.log(links);
